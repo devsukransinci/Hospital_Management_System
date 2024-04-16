6da31b342b53381a53417e2b11cfb1065e2014b5
şükran sinci <80584011+devsukransinci@users.noreply.github.com>
2024-04-17 02:00:24 +0300

    Delete src/main/java/com/HospitalManagement/repository/PatientRepository.java

--- src/main/java/com/HospitalManagement/repository/PatientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.HospitalManagement.repository;
-
-import com.HospitalManagement.entity.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PatientRepository extends JpaRepository<Patient,Long> {
-}
