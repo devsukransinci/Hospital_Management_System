4dca06ba741c68147f95b55a98de41a9add3e36f
şükran sinci <80584011+devsukransinci@users.noreply.github.com>
2024-04-18 01:16:53 +0300

    Add files via upload

---
+++ frontend/doctorauthguard.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { DocauthService } from '../app/docauth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DoctorauthguardService implements CanActivate{
+
+  constructor(private docAuthService:DocauthService,private router:Router) { }
+
+   canActivate(){
+      if(this.docAuthService.isUserLoggedIn()){
+      return true;
+  }
+  else{
+    this.router.navigate(['doclogin'])
+    return false;
+  }
+
+
+   }
+ 
+}
+
